05de92aa2321a6ed99de69bf2360ba431eac5b5e
TropheusJ <jverrellijr@icloud.com>
2022-01-09 04:14:20 -0500

    just need to push

--- src/main/java/net/ignoramuses/bingBingWahoo/cap/MysteriousCapFeatureRenderer.java
+++ src/main/java/net/ignoramuses/bingBingWahoo/cap/MysteriousCapFeatureRenderer.java
@@ -19,6 +19,9 @@ import static net.ignoramuses.bingBingWahoo.BingBingWahoo.MYSTERIOUS_CAP;
 
 @Environment(EnvType.CLIENT)
 public class MysteriousCapFeatureRenderer<T extends Entity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
+	public static final Identifier CAP = new Identifier(BingBingWahoo.ID, "textures/armor/mysterious_cap.png");
+	public static final Identifier EMBLEM = new Identifier(BingBingWahoo.ID, "textures/armor/mysterious_cap_emblem.png");
+	
 	private final MysteriousCapModel model;
 	
 	public MysteriousCapFeatureRenderer(FeatureRendererContext<T, M> context, MysteriousCapModel model) {
@@ -38,33 +41,46 @@ public class MysteriousCapFeatureRenderer<T extends Entity, M extends EntityMode
 		float r = (color >> 16 & 255) / 255.0F;
 		float g = (color >> 8 & 255) / 255.0F;
 		float b = (color & 255) / 255.0F;
-		matrices.push();
+//		matrices.push();
 		model.wearerHead.rotate(matrices);
-		final AtomicDouble x = new AtomicDouble(0);
-		final AtomicDouble y = new AtomicDouble(0);
-		final AtomicDouble z = new AtomicDouble(0);
-		final AtomicDouble lowest = new AtomicDouble(Integer.MAX_VALUE);
-		final AtomicDouble highest = new AtomicDouble(Integer.MIN_VALUE);
+		final AtomicDouble xLow = new AtomicDouble(Double.MAX_VALUE);
+		final AtomicDouble xHigh = new AtomicDouble(Double.MIN_VALUE);
+		final AtomicDouble yLow = new AtomicDouble(Double.MAX_VALUE);
+		final AtomicDouble yHigh = new AtomicDouble(Double.MIN_VALUE);
+		final AtomicDouble zLow = new AtomicDouble(Double.MAX_VALUE);
+		final AtomicDouble zHigh = new AtomicDouble(Double.MIN_VALUE);
 		model.wearerHead.forEachCuboid(matrices, (matrix, path, index, cuboid) -> {
-			if (path == null || path.isEmpty() || path.contains("head")) {
-				x.set(cuboid.maxX - cuboid.minX);
-				y.set(cuboid.maxY - cuboid.minY);
-				z.set(cuboid.maxZ - cuboid.minZ);
+			if (path == null || path.isEmpty() || path.equals("/") || path.contains("head")) {
+				xLow.set(cuboid.minX);
+				xHigh.set(cuboid.maxX);
+				yLow.set(cuboid.minY);
+				yHigh.set(cuboid.maxY);
+				zLow.set(cuboid.minZ);
+				zHigh.set(cuboid.maxZ);
 			}
-			if (cuboid.minY < lowest.get()) {
-				lowest.set(cuboid.minY);
-			}
-			if (cuboid.maxY > highest.get()) {
-				highest.set(cuboid.maxY);
-			}
+//			if (cuboid.minY < lowest.get()) {
+//				lowest.set(cuboid.minY);
+//			}
+//			if (cuboid.maxY > highest.get()) {
+//				highest.set(cuboid.maxY);
+//			}
 		});
-		double totalHeight = highest.get() - lowest.get();
-		double size = Math.min(x.get(), Math.min(y.get(), z.get()));
+//		double totalHeight = highest.get() - lowest.get();
+//		double size = 1 / Math.min(x.get(), Math.min(y.get(), z.get()));
 //		matrices.scale((float) size, (float) size, (float) size);
-		matrices.translate(0, -1.9, 0);
-		matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(90));
-		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(new Identifier(BingBingWahoo.ID, "textures/armor/mysterious_cap.png"))), light, 1, r, g, b, 1);
-		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(new Identifier(BingBingWahoo.ID, "textures/armor/mysterious_cap_emblem.png"))), light, 1, 1, 1, 1, 1);
+		
+		matrices.push();
+		matrices.translate(xLow.get(), yLow.get(), zLow.get());
+		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(CAP)), light, 1, r, g, b, 1);
 		matrices.pop();
+		matrices.push();
+		matrices.translate(xHigh.get(), yHigh.get(), zHigh.get());
+		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(CAP)), light, 1, r, g, b, 1);
+		matrices.pop();
+//		matrices.translate(0, -1.9, 0);
+//		matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(90));
+//		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(CAP)), light, 1, r, g, b, 1);
+//		model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getArmorCutoutNoCull(EMBLEM)), light, 1, 1, 1, 1, 1);
+//		matrices.pop();
 	}
 }

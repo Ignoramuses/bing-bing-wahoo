a9b61de128e7cf959eda1f17eea74d6f46a6416c
TropheusJ <jverrellijr@icloud.com>
2023-08-29 18:01:24 -0400

    port more stuff

--- src/main/java/io/github/ignoramuses/bing_bing_wahoo/BingBingWahooClient.java
+++ src/main/java/io/github/ignoramuses/bing_bing_wahoo/BingBingWahooClient.java
@@ -5,17 +5,18 @@ import io.github.ignoramuses.bing_bing_wahoo.content.cap.render.CapArmorRenderer
 import io.github.ignoramuses.bing_bing_wahoo.registry.WahooItems;
 import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.fabricmc.loader.api.FabricLoader;
 import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.loader.api.QuiltLoader;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class BingBingWahooClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient(ModContainer mod) {
+		//noinspection deprecation - not a full replacement
 		ArmorRenderer.register(CapArmorRenderer.INSTANCE, WahooItems.MYSTERIOUS_CAP);
 		ColorProviderRegistry.ITEM.register(WahooItems.MYSTERIOUS_CAP::getColor, WahooItems.MYSTERIOUS_CAP);
 
-		if (FabricLoader.getInstance().isModLoaded("trinkets")) {
+		if (QuiltLoader.isModLoaded("trinkets")) {
 			TrinketsCompat.init();
 		}
 	}
